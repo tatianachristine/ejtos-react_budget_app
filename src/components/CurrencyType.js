ce07b28ddc72cfcd0388270b07c3e7ce148c583d
tatianachristine <tatianachristinee@gmail.com>
2023-12-09 16:30:41 -0500

    commit on 'main'

--- src/components/CurrencyType.js
+++ src/components/CurrencyType.js
@@ -1,18 +1,26 @@
-//import React, { useContext, useState } from 'react';
-//import { AppContext } from '../context/AppContext';
+import React, { useContext/*, useState */ } from 'react';
+import { AppContext } from '../context/AppContext';
 
 const CurrencyType = () => {
+    const { dispatch } = useContext(AppContext);
+
+    //const [type, setCurrencyType] = useState('');
+
+    const submitEvent = () => {
+        dispatch({
+            type: 'CHG_CURRENCY',
+            payload: document.getElementById('inputGroupSelect03').value,
+        });
+    }
 
     return (
-        <div className="alert alert-success">
-            <select className="custom-select" id="inputGroupSelect03">
-                <option defaultValue>Currency</option>
-                <option value="Dollar" name="dollar"> Dollar</option>
-                <option value="Pound" name="pound">Pound</option>
-                <option value="Euro" name="euro">Euro</option>
-                <option value="Ruppee" name="ruppee">Ruppee</option>
-            </select>
-        </div>
+        <select className="alert alert-success" id="inputGroupSelect03" onChange={submitEvent}>
+            <option defaultValue>Currency</option>
+            <option value="$" name="dollar">$ Dollar</option>
+            <option value="£" name="pound">£ Pound</option>
+            <option value="€" name="euro">€ Euro</option>
+            <option value="₹" name="rupee">₹ Rupee</option>
+        </select>
     );
     };
 
--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -70,6 +70,7 @@ export const AppReducer = (state, action) => {
         case 'CHG_CURRENCY':
             action.type = "DONE";
             state.currency = action.payload;
+            
             return {
                 ...state
             }
