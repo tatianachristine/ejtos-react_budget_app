6898991342c3d7e9a35802821572395b88e5fdcc
Tatiana <144362950+tatianachristine@users.noreply.github.com>
2023-12-11 17:51:54 -0500

    updates made at home

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,8 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
-import ReactDOM from 'react-dom/client';
-import Remaining from './Remaining';
 
 const Budget = () => {
-    const { budget, expenses } = useContext(AppContext);
+    const { budget, expenses, currency, dispatch } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const totalExpenses = expenses.reduce((total, item) => {
         return (total += item.cost);
@@ -14,14 +12,18 @@ const Budget = () => {
             alert("You cannot reduce the budget value below the spending");
         } else {
         setNewBudget(event.target.value);
-        ReactDOM.render(<Remaining />, document.getElementById("Remaining"))
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: document.getElementById('budget_input').value,
+        });
         }
     }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £</span>
-<input type="number" step="10" max="20000" value={newBudget} onChange={handleBudgetChange}></input>
+<span>Budget: {currency}</span>
+<input type="number" step="10" max="20000" id="budget_input" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
 };
 export default Budget;
+
